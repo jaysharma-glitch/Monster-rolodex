135e73ab3813c5e72744e2a12e14985f765aab6e
Jay <jayzzzzzsharma@gmail.com>
2020-07-15 17:15:05 +0530

    Updates

---
+++ precache-manifest.3522565967465f60ad49faca6b69ae9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99068396f7a19b3ab01ffba567fde35e",
+    "url": "/Monster-rolodex/index.html"
+  },
+  {
+    "revision": "9e43cf39e276219811c2",
+    "url": "/Monster-rolodex/static/css/main.44633210.chunk.css"
+  },
+  {
+    "revision": "efe7611b8fa5e7cf6d03",
+    "url": "/Monster-rolodex/static/js/2.e2c71172.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/Monster-rolodex/static/js/2.e2c71172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e43cf39e276219811c2",
+    "url": "/Monster-rolodex/static/js/main.d40a7cfb.chunk.js"
+  },
+  {
+    "revision": "405800b5fe7a1c0159aa",
+    "url": "/Monster-rolodex/static/js/runtime-main.3102fe84.js"
+  }
+]);
